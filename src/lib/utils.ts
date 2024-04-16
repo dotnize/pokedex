1d4308fc06f0110a7e499885bf280866b2cec3c6
Nathaniel Tampus <nate@mugnavo.com>
2024-04-17 02:02:05 +0800

    add utils for metric conversion - hg to kg - dm to m

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,11 @@ export function formatDigits(num: number) {
 export function capitalize(str: string) {
 	return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function hgToKg(hg: number) {
+	return hg / 10;
+}
+
+export function dmToM(dm: number) {
+	return dm / 10;
+}
