c84c5b7244a2b642e833872857ba3b54d953bd83
Nathaniel Tampus <nate@mugnavo.com>
2024-04-16 23:15:33 +0800

    add loader to homepage to fetch initial pokemons

---
+++ src/routes/+page.ts
@@ -0,0 +1,28 @@
+import type { Pokemon } from "$lib/types";
+
+interface PokemonsResponse {
+	count: number;
+	next: string | null;
+	previous: string | null;
+	results: Pokemon[];
+}
+
+export async function load() {
+	// TODO: pagination
+	const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+	const pokemons: PokemonsResponse = await response.json();
+
+	// add id and image to each pokemon based on URL
+	// much more efficient than fetching for each pokemon, we can just get images from PokeAPI's repo
+	pokemons.results = pokemons.results.map((pokemon) => {
+		const id = parseInt(pokemon.url.split("/")[6]);
+		return {
+			...pokemon,
+			id,
+			imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+		};
+	});
+
+	console.log(pokemons);
+	return { pokemons };
+}
