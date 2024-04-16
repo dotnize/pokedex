2b0b906db9a27aeb1693a446178ccffa2f93dbb8
Nathaniel Tampus <nate@mugnavo.com>
2024-04-16 23:14:46 +0800

    add pokemon card component for homepage

---
+++ src/lib/components/PokemonCard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Pokemon } from "$lib/types";
+	import { capitalize, formatDigits } from "$lib/utils";
+
+	export let pokemon: Pokemon;
+</script>
+
+<a
+	href={`/details/${pokemon.id}`}
+	class="flex flex-col items-center rounded-lg border-2 p-4 transition hover:scale-[1.02]"
+>
+	<img height={144} width={144} src={pokemon.imageUrl} alt={`Picture of ${pokemon.name}`} />
+	<span class="text-gray-500">{formatDigits(pokemon.id)}</span>
+	{capitalize(pokemon.name)}
+</a>
