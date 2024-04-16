dd96425782d3d40cd6d37751df1c5649b988856d
Nathaniel Tampus <nate@mugnavo.com>
2024-04-16 23:32:45 +0800

    add site title to head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import "../app.css";
 </script>
 
+<svelte:head>
+	<title>Nate's Pokedex</title>
+</svelte:head>
+
 <header class="flex h-32 flex-col items-center justify-center gap-4">
 	<a href="/" class="text-2xl font-bold">Pokedex</a>
 	<span>work in progress, will also explore other ui styles and add icons(?)</span>
