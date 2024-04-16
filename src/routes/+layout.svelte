ba5a42fc989a0fea22fd3a4fdceb356f5b79943e
Nathaniel Tampus <nate@mugnavo.com>
2024-04-16 23:14:19 +0800

    add initial layout with title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,11 @@
 	import "../app.css";
 </script>
 
-<slot />
+<header class="flex h-32 flex-col items-center justify-center gap-4">
+	<a href="/" class="text-2xl font-bold">Pokedex</a>
+	<span>work in progress, will also explore other ui styles and add icons(?)</span>
+</header>
+
+<main>
+	<slot />
+</main>
