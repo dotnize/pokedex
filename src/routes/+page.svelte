77f3f86dad81105c481ee048999963fc90b914ee
Nathaniel Tampus <nate@mugnavo.com>
2024-04-16 23:17:34 +0800

    initial homepage with card components & loader data - wip, currently just placeholder buttons for sorting - no details page yet when clicking a card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,28 @@
-<h1 class="text-2xl font-bold">Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://kit.svelte.dev" class="text-blue-500 underline">kit.svelte.dev</a> to read the
-	documentation
-</p>
+<script lang="ts">
+	import PokemonCard from "$lib/components/PokemonCard.svelte";
+
+	export let data;
+
+	const pokemons = data.pokemons.results;
+</script>
+
+<div class="flex flex-col items-center justify-center gap-4 px-4 py-8">
+	<div class="flex w-full max-w-screen-md flex-col items-center justify-between gap-4 sm:flex-row">
+		<input type="text" placeholder="Search..." class="w-full rounded-md border-2 p-2 sm:max-w-sm" />
+		<div class="flex w-full items-center justify-end gap-1">
+			Sort by:
+			<div class="flex">
+				<button class="w-16 rounded-l-md border-2 border-r-0 bg-blue-400 p-1 transition">ID</button>
+				<button class="w-16 rounded-r-md border-2 border-l-0 p-1 transition hover:bg-blue-200"
+					>Name</button
+				>
+			</div>
+			<button class="rounded-md border p-1">asc/desc icon</button>
+		</div>
+	</div>
+	<div class="grid w-full max-w-screen-md gap-4 sm:grid-cols-2 lg:grid-cols-3">
+		{#each pokemons as pokemon}
+			<PokemonCard {pokemon} />
+		{/each}
+	</div>
+</div>
