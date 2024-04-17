a4774d28b39e43fdc600df16edb533ba0dd4fe6a
Nathaniel Tampus <nate@mugnavo.com>
2024-04-17 17:26:16 +0800

    add borders to elements on details page for depth

--- src/routes/[name]/+page.svelte
+++ src/routes/[name]/+page.svelte
@@ -8,24 +8,24 @@
 
 	// map types to tailwind colors, from copilot :D
 	const colors = new Map([
-		["normal", "bg-purple-400"],
-		["fire", "bg-red-300"],
-		["water", "bg-blue-500"],
-		["electric", "bg-yellow-500"],
-		["grass", "bg-green-500"],
-		["ice", "bg-blue-200"],
-		["fighting", "bg-red-400"],
-		["poison", "bg-purple-500"],
-		["ground", "bg-yellow-800 text-purple-200"],
-		["flying", "bg-blue-300"],
-		["psychic", "bg-purple-200"],
-		["bug", "bg-green-200"],
-		["rock", "bg-yellow-400"],
-		["ghost", "bg-purple-800 text-purple-200"],
-		["dragon", "bg-blue-800 text-purple-200"],
-		["dark", "bg-gray-800 text-purple-200"],
-		["steel", "bg-gray-600 text-purple-200"],
-		["fairy", "bg-pink-300"],
+		["normal", "bg-purple-400 border-purple-500"],
+		["fire", "bg-red-300 border-red-400"],
+		["water", "bg-blue-500 border-blue-600"],
+		["electric", "bg-yellow-500 border-yellow-600"],
+		["grass", "bg-green-500 border-green-600"],
+		["ice", "bg-blue-200 border-blue-300"],
+		["fighting", "bg-red-400 border-red-500"],
+		["poison", "bg-purple-500 border-purple-600"],
+		["ground", "bg-yellow-800 text-purple-200 border-yellow-900"],
+		["flying", "bg-blue-300 border-blue-400"],
+		["psychic", "bg-purple-200 border-purple-400"],
+		["bug", "bg-green-200 border-green-300"],
+		["rock", "bg-yellow-400 border-yellow-500"],
+		["ghost", "bg-purple-800 text-purple-200 border-purple-900"],
+		["dragon", "bg-blue-800 text-purple-200 border-blue-900"],
+		["dark", "bg-gray-800 text-purple-200 border-gray-900"],
+		["steel", "bg-gray-600 text-purple-200 border-gray-700"],
+		["fairy", "bg-pink-300 border-pink-400"],
 	]);
 </script>
 
@@ -81,7 +81,9 @@
 				<h2 class="text-2xl font-bold">{capitalize(pokemon.name)}</h2>
 				<span class=""># <span class="text-xl font-bold">{formatDigits(pokemon.id)}</span></span>
 			</div>
-			<div class="flex h-full w-full items-center justify-center rounded-xl bg-purple-200">
+			<div
+				class="flex h-full w-full items-center justify-center rounded-xl border-b-2 border-r-2 border-purple-400 bg-purple-200"
+			>
 				<img
 					height={768}
 					width={768}
@@ -104,8 +106,9 @@
 				<h3 class="text-xl font-bold">Types</h3>
 				<div class="flex flex-wrap gap-2">
 					{#each pokemon.types as type}
-						<span class={"min-w-16 rounded-md px-2 py-1 text-center " + colors.get(type.type.name)}
-							>{capitalize(type.type.name)}</span
+						<span
+							class={"min-w-16 rounded-md border-b-2 border-r-2 px-2 py-1 text-center " +
+								colors.get(type.type.name)}>{capitalize(type.type.name)}</span
 						>
 					{/each}
 				</div>
@@ -114,7 +117,7 @@
 				<h3 class="text-xl font-bold">Abilities</h3>
 				<div class="flex flex-wrap gap-1">
 					{#each pokemon.abilities as ability}
-						<span class="rounded-md bg-purple-400 px-2 py-1"
+						<span class="rounded-md border-b-2 border-r-2 border-purple-500 bg-purple-400 px-2 py-1"
 							>{capitalize(ability.ability.name)}
 							{#if ability.is_hidden}
 								<span class="text-xs text-purple-800"> (hidden)</span>
@@ -128,13 +131,13 @@
 				<h3 class="text-xl font-bold">Stats</h3>
 				<!-- stats for smaller screens, vertical bar chart -->
 				<div
-					class="hidden h-40 w-full items-end gap-2 rounded-md bg-purple-200 px-2 text-purple-950 sm:flex"
+					class="hidden h-40 w-full items-end gap-2 rounded-md border-b-2 border-r-2 border-purple-400 bg-purple-200 px-2 text-purple-950 sm:flex"
 				>
 					{#each pokemon.stats as stat}
 						<div class="flex flex-1 flex-col items-center text-center font-mono">
 							<span class="text-xs">{stat.stat.name}</span>
 							<div
-								class={`flex w-full items-center justify-center rounded-t bg-purple-400`}
+								class={`flex w-full items-center justify-center rounded-t border-r-2 border-purple-500 bg-purple-400`}
 								style={`height: ${stat.base_stat}px`}
 							>
 								<span class="text-xs">{stat.base_stat}</span>
@@ -147,7 +150,7 @@
 					{#each pokemon.stats as stat}
 						<div class="flex flex-1 items-center gap-0.5 text-center font-mono">
 							<div
-								class={`flex h-8 w-full items-center justify-center rounded-r bg-purple-400`}
+								class={`flex h-8 w-full items-center justify-center rounded-r border-b-2 border-r-2 border-purple-500 bg-purple-400`}
 								style={`width: ${stat.base_stat}px`}
 							>
 								<span class="text-xs">{stat.base_stat}</span>
@@ -162,8 +165,9 @@
 				<h3 class="text-xl font-bold">Weaknesses</h3>
 				<div class="flex flex-wrap gap-1">
 					{#each pokemon.weaknesses as weakness}
-						<span class={"min-w-16 rounded-md px-2 py-1 text-center " + colors.get(weakness.name)}
-							>{capitalize(weakness.name)}</span
+						<span
+							class={"min-w-16 rounded-md border-b-2 border-r-2 px-2 py-1 text-center " +
+								colors.get(weakness.name)}>{capitalize(weakness.name)}</span
 						>
 					{/each}
 				</div>
