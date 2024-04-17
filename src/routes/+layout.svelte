7cb5ff6303cc99dc47660937f38c8fbaa868605c
Nathaniel Tampus <nate@mugnavo.com>
2024-04-17 17:10:12 +0800

    add loading spinner for navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,27 @@
 <script>
 	import "@fontsource/press-start-2p";
-
 	import "../app.css";
+
+	import { navigating } from "$app/stores";
+	import { LoaderCircle } from "lucide-svelte";
 </script>
 
 <svelte:head>
 	<title>Nate's Pokedex</title>
 </svelte:head>
 
-<header class="flex h-32 flex-col items-center justify-center font-title">
+<header class="flex h-36 flex-col items-center justify-center gap-2 font-title">
 	<div class="flex flex-col text-purple-950">
 		nate's
 		<a href="/" class="rounded bg-purple-950 p-2 text-3xl font-bold text-purple-400"
 			>Poke<span class="text-purple-50">dex</span></a
 		>
+	</div>
+
+	<div class="h-7">
+		{#if $navigating}
+			<LoaderCircle class="animate-spin text-purple-900" size={28} />
+		{/if}
 	</div>
 </header>
 
